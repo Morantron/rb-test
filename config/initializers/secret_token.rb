15b41c271b5e4a6d0adf9312ff5467fb81609b98
Jorge Morante <jorge@morante.eu>
2014-02-25 21:45:14 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedboothTest::Application.config.secret_key_base = '387f3db104d5365f04a85700fcec3ec03f5f6468f81a9fc3b031a30b31bd9b60106946ba063cb30432d7d16121198666b4988fa1ebce08f687562e0ad5e35a62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RedboothTest::Application.config.session_store :cookie_store, key: '_redbooth-test_session'
